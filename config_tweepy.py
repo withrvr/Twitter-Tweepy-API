98282d0b3fd288fd49e4504b8b68a2beda29a51f
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2021-02-22 19:03:53 +0530

    config file ( Comman code )

--- config_tweepy.py
+++ config_tweepy.py
@@ -18,4 +18,6 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # initialize Tweepy API
-api = tweepy.API(auth)
+# api = tweepy.API(auth)
+# api = tweepy.API(auth, wait_on_rate_limit=True)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
